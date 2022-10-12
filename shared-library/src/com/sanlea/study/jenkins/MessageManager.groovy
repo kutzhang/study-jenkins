e31ce44df9ff1170e4dc6a6bb85b4d6145205271
Kut Zhang <kut@sanlea.cn>
2022-10-12 21:16:37 +0800

    sadfaf

--- shared-library/src/com/sanlea/study/jenkins/MessageManager.groovy
+++ shared-library/src/com/sanlea/study/jenkins/MessageManager.groovy
@@ -1,7 +1,7 @@
 package com.sanlea.study.jenkins
 
 class MessageManager {
-    static def say(script, message) {
-        script.sh "message manager says: ${message}"
+    static def say(script, content) {
+        script.sh "message manager says: ${content}"
     }
 }
