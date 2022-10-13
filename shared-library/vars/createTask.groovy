70af5e09af60b62c220db66034a1f706fa867fb8
Kut Zhang <kut@sanlea.cn>
2022-10-13 15:31:58 +0800

    fasdfasdf

--- shared-library/vars/createTask.groovy
+++ shared-library/vars/createTask.groovy
@@ -1,6 +1,6 @@
 import om.sanlea.study.jenkins.component.ApiClient
 
-void createTask(String projectName, Closure closure) {
+void call(String projectName, Closure closure) {
     def apiClient = new ApiClient()
     def response = apiClient.request("http://192.168.10.174:8090/api/ci/task", "POST", [
         "projectName": projectName
